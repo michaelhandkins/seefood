cb428f540ed93da3be8f58f645bc614d0c77cd81
Michael Handkins <michaelhandkins@gmail.com>
2020-11-09 11:09:15 -0600

    Create and implement function that gets image recognition results back from V3 model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -29,10 +29,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Error loading results from request")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Error performing request with handler: \(error)")
+        }
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
